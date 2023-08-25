9684b17e7d06da0d7ae7d1d8ecdb6b47aebe110c
EBOKE BLANDINE <ebokeblandine2@gmail.com>
2023-08-25 14:43:05 +0100

    Add project files.

---
+++ my c# beginig/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+string firstFriend = "Susan";
+string secondFriend = "Arrey";
+
+Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
